94f8c9f48c404e1c61b3045e4244a5e89c106f73
ILIASSTAJ <iliassbasrtaj@gmail.com.com>
2023-07-24 05:30:48 -0700

    starting

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>  //use the built in functions if it not work remove the lib and code it yourself (strlen/strcpy....)
+
